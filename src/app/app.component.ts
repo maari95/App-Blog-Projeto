1640976935a5d5f19236b5f39fb4b87a11a9eb57
João Pedro <joao3001int@gmail.com>
2023-09-25 15:30:01 -0300

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,11 @@ export class AppComponent {
     { title: 'Nova postagem', url: 'envio-post', icon: 'add' },
     { title: 'Contato', url: 'contact', icon: 'chatbox-ellipses' },
     { title: 'Sobre', url: 'sobre', icon: 'information' },
-    { title: 'Login', url: 'login', icon: 'person' },
+    
     
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+  
+
   constructor() {}
 }
--- src/app/page/login/login.page.spec.ts
+++ src/app/page/login/login.page.spec.ts
@@ -5,11 +5,7 @@ describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
-  beforeEach(async(() => {
-    fixture = TestBed.createComponent(LoginPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+  
 
   it('should create', () => {
     expect(component).toBeTruthy();
